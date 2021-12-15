89ba4b763caae1b3d43a804401a00d0a5aed6946
Arun.R.R <arun90rr@gmail.com>
2021-12-15 20:12:56 +0530

    Add HTML structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import TodoItem from "$lib/todo-component.svelte";
+
+  const title = "TODO";
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+<form method="" action="">
+  <input
+    type="text"
+    name="text"
+    aria-label="Add Todo"
+    placeholder="+ Add TODO here"
+  />
+</form>
+
+<TodoItem />
+<TodoItem />
+<TodoItem />
